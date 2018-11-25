f00092a89e7b2341d2bd0e5972c100e574aefe85
thefenderstory <jgrady15@brophybroncos.org>
2018-11-24 20:38:58 -0700

    Initial travel trail view

---
+++ src/view/TrailTravelView.java
@@ -0,0 +1,67 @@
+package view;
+
+import javafx.scene.control.Alert.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.application.*;
+import javafx.scene.input.*;
+import javafx.scene.image.*;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
+import javafx.beans.value.*;
+import javafx.geometry.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.text.Font;
+import javafx.event.*;
+import java.util.*;
+import java.io.*;
+import javafx.scene.text.Text;
+import javafx.scene.input.KeyCode;
+
+import controller.*;
+
+public class TrailTravelView extends Scene {
+
+  /**
+   * [TrailTravelView description]
+   */
+  public TrailTravelView() {
+    this(new BorderPane());
+    getStylesheets().add(AZTrailView.styleSheet);
+  }
+
+  /**
+   * [TrailTravelView description]
+   * @param root [description]
+   */
+  private TrailTravelView(BorderPane root) {
+    super(root, AZTrailView.WIDTH, AZTrailView.HEIGHT);
+
+    Text cont = new Text("Press SPACEBAR to continue...");
+    cont.setId("text12");
+    cont.setFill(Color.WHITE);
+
+    BorderPane info = infoPane();
+    root.setStyle("-fx-background-color: black;");
+    root.setAlignment(cont, Pos.CENTER);
+    //root.setTop();
+    root.setCenter(info);
+    root.setBottom(cont);
+
+  }
+
+  private BorderPane infoPane() {
+    BorderPane info = new BorderPane();
+    // TODO get partyStats from controller
+    HBox statsArea = new HBox();
+    Text stats = new Text("Weather: cold\nHealth: good\nFood: 0 Pounds\nNext landmark: 44 miles\nMiles Traveled: 0 miles");
+
+    statsArea.getChildren().add(stats);
+    statsArea.setStyle("-fx-background-color: white;");
+
+    info.setAlignment(statsArea, Pos.CENTER);
+    info.setBottom(statsArea);
+    return info;
+  }
+}
