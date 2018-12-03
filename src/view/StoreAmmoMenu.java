24c9d96bcd6c9f322e137d6af0bdf15102355e01
enajork <eric@najork.org>
2018-12-02 18:07:26 -0700

    bug fixes

--- src/view/StoreAmmoMenu.java
+++ src/view/StoreAmmoMenu.java
@@ -44,19 +44,19 @@ public class StoreAmmoMenu extends Scene {
    * [StoreAmmoMenu description]
    */
   public StoreAmmoMenu(String name, boolean start) {
-    this(new BorderPane());
+    this(new BorderPane(), name, start);
     getStylesheets().add(AZTrailView.styleSheet);
     AZTrailController.escape = false;
-    this.name = name;
-    this.start = start;
   }
 
   /**
    * [StoreAmmoMenu description]
    * @param root [description]
    */
-  private StoreAmmoMenu(BorderPane root) {
+  private StoreAmmoMenu(BorderPane root, String name, boolean start) {
     super(root, AZTrailView.WIDTH, AZTrailView.HEIGHT, Color.BLACK);
+    this.name = name;
+    this.start = start;
     tile = new BorderPane();
     tile.setStyle("-fx-background-color: black;");
     BorderPane banner = new BorderPane();
