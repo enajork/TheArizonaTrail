4f090775e527e0716ff5454dc91b71e047ca28b7
enajork <eric@najork.org>
2018-12-02 19:20:32 -0700

    added game over screen

---
+++ src/view/TombstoneMenu.java
@@ -0,0 +1,111 @@
+package view;
+
+import javafx.scene.control.Alert.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.input.*;
+import javafx.scene.text.*;
+import javafx.application.*;
+import javafx.scene.input.*;
+import javafx.scene.image.*;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
+import javafx.scene.*;
+import javafx.beans.value.*;
+import javafx.geometry.*;
+import javafx.stage.*;
+import javafx.event.*;
+import java.util.*;
+import java.io.*;
+
+import controller.*;
+
+public class TombstoneMenu extends Scene {
+  private BorderPane root;
+
+  /**
+   * [TombstoneMenu description]
+   */
+  public TombstoneMenu() {
+    this(new BorderPane());
+    getStylesheets().add(AZTrailView.styleSheet);
+  }
+
+  /**
+   * [TombstoneMenu description]
+   * @param root [description]
+   */
+  private TombstoneMenu(BorderPane root) {
+    super(root, AZTrailView.WIDTH, AZTrailView.HEIGHT, Color.BLACK);
+    getStylesheets().add(AZTrailView.styleSheet);
+    AZTrailView.sounds.stopMovingSFX();
+    AZTrailView.sounds.startThemeLoop();
+    this.root = root;
+    root.setStyle("-fx-background-color: black;");
+
+    Image img = new Image((AZTrailView.controller.getHunted())
+      ? "file:view/assets/graphics/here-lies-hunted.png"
+      : "file:view/assets/graphics/here-lies.png");
+    ImageView splash = new ImageView(img);
+    splash.setPreserveRatio(true);
+    // splash.setFitWidth(AZTrailView.WIDTH * 0.985);
+    root.setMargin(splash, new Insets(5, 5, 0, 5));
+    root.setAlignment(splash, Pos.CENTER);
+    root.setTop(splash);
+    Text text = new Text("Game over!");
+    text.setId("text12");
+    text.setFill(Color.BLACK);
+    StackPane stack = new StackPane();
+    stack.getChildren().add(splash);
+    stack.getChildren().add(text);
+    root.setCenter(stack);
+    root.setAlignment(stack, Pos.CENTER);
+    root.setMargin(stack, new Insets(2));
+
+    // Style the view
+    Text footer = new Text("Press SPACE BAR to continue");
+    footer.setId("text12");
+    footer.setFill(Color.WHITE);
+    root.setBottom(footer);
+    root.setAlignment(footer, Pos.CENTER);
+    root.setMargin(footer, new Insets(0, 0, 20, 0));
+
+    addEventHandlers();
+  }
+
+  /**
+   * [addEventHandlers description]
+   */
+  private void addEventHandlers() {
+    this.setOnKeyPressed(new EventHandler<KeyEvent>() {
+      @Override
+      public void handle(KeyEvent event) {
+        switch (event.getCode()) {
+          case SPACE:
+            AZTrailController.escape = false;
+            AZTrailView.stage.setScene(new SplashMenu());
+            break;
+
+          case ENTER:
+            AZTrailController.escape = false;
+            AZTrailView.stage.setScene(new SplashMenu());
+            break;
+
+          case ESCAPE:
+            AZTrailView.escapePressed(false);
+            break;
+          case S:
+            AZTrailController.escape = false;
+            if (event.isControlDown()) {
+              AZTrailView.sounds.mute();
+            }
+            break;
+
+          default:
+            AZTrailController.escape = false;
+            return;
+        }
+      }
+    });
+  }
+}
